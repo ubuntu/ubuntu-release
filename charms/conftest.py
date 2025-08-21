d547b3beb2341c37ed0a4d694d2face1b151c7a7
Florent 'Skia' Jacquet <florent.jacquet@canonical.com>
2025-08-21 12:09:43 +0200

    Kickstart 'worker' charm

---
+++ charms/conftest.py
@@ -0,0 +1,22 @@
+import logging
+from pathlib import Path
+
+import jubilant
+import pytest
+
+logger = logging.getLogger()
+
+
+def charm_path(name: str) -> Path:
+    """Return full absolute path to given test charm."""
+    charm_dir = Path(__file__).parent / name
+    charms = [p.absolute() for p in charm_dir.glob(f"ubuntu-release-{name}_*.charm")]
+    assert charms, f"ubuntu-release-{name}_*.charm not found"
+    assert len(charms) == 1, "more than one .charm file, unsure which to use"
+    return charms[0]
+
+
+@pytest.fixture(scope="module")
+def juju() -> jubilant.Juju:
+    with jubilant.temp_model() as juju:
+        yield juju
---
+++ charms/worker/tests/test_worker.py
@@ -0,0 +1,15 @@
+import logging
+
+import jubilant
+from conftest import charm_path
+
+logger = logging.getLogger()
+
+
+def test_deploy(juju: jubilant.Juju):
+    juju.deploy(
+        charm=charm_path("worker"),
+        app="worker",
+    )
+
+    juju.wait(lambda status: jubilant.all_active(status, "worker"), timeout=600)
