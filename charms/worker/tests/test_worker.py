546a56acf4a99e5d03857f4fbbebe48070dd3174
Florent 'Skia' Jacquet <florent.jacquet@canonical.com>
2025-08-21 17:15:26 +0200

    WIP

--- charms/worker/tests/test_worker.py
+++ charms/worker/tests/test_worker.py
@@ -7,10 +7,14 @@ from conftest import charm_path
 logger = logging.getLogger()
 
 
-def test_deploy(juju: jubilant.Juju):
+def test_deploy(juju: jubilant.Juju, repo_sha: str):
     juju.deploy(
         charm=charm_path("worker"),
         app="worker",
+        resources={
+            "ubuntu-release-worker": "ghcr.io/ubuntu/ubuntu-release/ubuntu-release-worker:"
+            + repo_sha
+        },
     )
 
     juju.wait(lambda status: jubilant.all_active(status, "worker"), timeout=600)
