72477cd76dd87d1183ab7b153ffdbd859f794c10
Florent 'Skia' Jacquet <florent.jacquet@canonical.com>
2025-08-28 10:17:13 +0200

    go: kickstart ubuntu-release{,worker} binaries with first Temporal workflow

--- ubuntu-release/main.go
+++ ubuntu-release/main.go
@@ -2,11 +2,16 @@
 Copyright © 2025 Utkarsh Gupta <utkarsh@ubuntu.com>
 */
 
-package cmd
+package main
 
 import (
-	"github.com/spf13/cobra"
+	"context"
+	"log"
 	"os"
+	"ubuntu-release/lib"
+
+	"github.com/spf13/cobra"
+	"go.temporal.io/sdk/client"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -23,6 +28,42 @@ var rootCmd = &cobra.Command{
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+var helloCmd = &cobra.Command{
+	Use:   "hello",
+	Short: "Trigger a Hello Ubuntu workflow",
+	Long:  `The Hello Ubuntu workflow says Hello to a given product`,
+	Run: func(cmd *cobra.Command, args []string) {
+		c, err := client.Dial(client.Options{})
+
+		if err != nil {
+			log.Fatalln("Unable to create Temporal client:", err)
+		}
+
+		defer c.Close()
+		options := client.StartWorkflowOptions{
+			ID:        "hello-ubuntu-666",
+			TaskQueue: lib.UbuntuReleaseTaskQueueName,
+		}
+
+		we, err := c.ExecuteWorkflow(context.Background(), options, lib.HelloUbuntu, "ubuntu-desktop")
+		if err != nil {
+			log.Fatalln("Unable to start the Workflow:", err)
+		}
+
+		log.Printf("WorkflowID: %s RunID: %s\n", we.GetID(), we.GetRunID())
+
+		var result string
+
+		err = we.Get(context.Background(), &result)
+
+		if err != nil {
+			log.Fatalln("Unable to get Workflow result:", err)
+		}
+
+		log.Println(result)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -44,4 +85,8 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(helloCmd)
+}
+func main() {
+	Execute()
 }
