f470f53a457864a3a697cc93b5cbfd63bf19c3b9
Jon Seager <jon@sgrs.uk>
2025-10-08 13:37:20 +0100

    test(charm): split deploy and ingress tests

---
+++ charms/worker/tests/integration/test_deploy.py
@@ -0,0 +1,11 @@
+import logging
+import jubilant
+
+logger = logging.getLogger()
+
+UR_WORKER = "ubuntu-release-worker"
+
+
+def test_deploy(juju: jubilant.Juju, worker_charm_path):
+    juju.deploy(charm=worker_charm_path, app=UR_WORKER)
+    juju.wait(lambda status: jubilant.all_active(status, UR_WORKER), timeout=1800)
